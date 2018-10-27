5ca86dea1d4686636792fb31a181f81880b98c70
Denise Lorenz <deniselorenz@Denises-MacBook-Pro.local>
2018-10-26 21:38:17 -0500

    got my api request to work and appending my images and ratings to the page

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -4,10 +4,11 @@ var topics = ["Friday", "Jif", "Spongebob Squarepants", "Hello", "selfie fails",
 
 
 function makeButtons() {
-    console.log("hey");
+    // console.log("hey");
 
     for (var i = 0; i < topics.length; i++) {
         var newButton = $("<button>").text(topics[i]);
+        $("newbutton").attr("data-search", topics[i]);
         $("#buttons").append(newButton);
     };
 }
@@ -16,27 +17,26 @@ makeButtons();
 
 
 $("button").on("click", function () {
-    console.log(($(this)).text());
-});
+    // console.log(($(this)).text());
 
 
 
 
 
-
-
-var searchGiffay = function (topics) {
-
-
-
-    var queryUrl = "https://www.giphy.com/?t=" + topics + "&y=&plot=short&apikey=trilogy"
-
-
+    var getGiphy = $(this).text();
+    var queryUrl = "https://api.giphy.com/v1/gifs/search?&q=" + getGiphy + "&api_key=JQsV5IHcXy0dqD8lbPQM6mpZwr8tg0Gv&limit=10";
+        // console.log(queryUrl);
+  
     $.ajax({
         url: queryUrl,
         method: "GET"
-    }).then(function (response) {
-        ;
-    });
-}
+    }).done(function (response) {
+        // console.log(response.data[0].rating);
+        for (var i = 0; i < response.data.length; i++) {
+        $("#postImages").prepend("<img src='" +response.data[i].images.fixed_height_still.url+ "'>");
+        $("#postImages").prepend("<p> Rating: " +response.data[i].rating + "</p>");
+        };
 
+    });
+
+});
